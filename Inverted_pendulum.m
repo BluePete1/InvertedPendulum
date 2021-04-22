c954d123114fc0fe74f51500073663540ae319a9
Elina Cherman <elina.cherman@student.kuleuven.be>
2021-04-22 16:06:06 +0200

    system

--- Inverted_pendulum.m
+++ Inverted_pendulum.m
@@ -1,15 +1,22 @@
 clear;
 
 p = parameters;
+%% Linear state space model
 
 A = [[0 0 1 0];
     [0 0 0 1];
-    [0 -p.m*p.g/p.M -p.K_g^2*p.K_m*p.K_b/(p.M*p.R_m*p.r^2) 0];
-    [0 (p.M+p.m)*p.g/(p.m*p.l) p.K_g^2*p.K_m*p.K_b/(p.M*p.R_m*p.r^2*p.l) 0]];
+    [0 -p.m*p.g/p.M -p.Kg^2*p.Km*p.Kb/(p.M*p.Rm*p.r^2) 0];
+    [0 (p.M+p.m)*p.g/(p.M*p.l) p.Kg^2*p.Km*p.Kb/(p.M*p.Rm*p.r^2*p.l) 0]];
+
 B = [0;
     0;
-    p.K_m*p.K_g/(p.M*p.R_m*p.r);
-    -p.K_m*p.K_g/(p.r*p.R_m*p.M*p.l)];
+    p.Km*p.Kg/(p.M*p.Rm*p.r);
+    -p.Km*p.Kg/(p.r*p.Rm*p.M*p.l)];
+
 C = [[1 0 0 0];
     [0 1 0 0]];
-D = 0;
+
+D = [0;0];
+
+sys = ss(A,B,C,D);
+
--- parameters.m
+++ parameters.m
@@ -1,9 +1,9 @@
 function p = parameters(varargin)
 
-p.R_m = 2.6; %Ohm
-p.K_m = 0.00767; %Nm/A
-p.K_b = 0.00767; %V/(rad*s)
-p.K_g = 3.7/1;
+p.Rm = 2.6; %Ohm
+p.Km = 0.00767; %Nm/A
+p.Kb = 0.00767; %V/(rad*s)
+p.Kg = 3.7/1;
 p.M = 0.455; %kg
 p.l = 0.305; %m
 p.m = 0.210; %kg
