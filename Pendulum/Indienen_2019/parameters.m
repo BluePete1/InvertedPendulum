05cdfd4c2b954980df92d22708dbcde1f1466b13
Brecht <brecht.geutjens@student.kuleuven.be>
2021-05-30 20:20:30 +0200

    2019 versie van indienen

---
+++ Pendulum/Indienen_2019/parameters.m
@@ -0,0 +1,13 @@
+function p = parameters(varargin)
+
+p.Rm = 2.6; %Ohm
+p.Km = 0.00767; %Nm/A
+p.Kb = 0.00767; %V/(rad*s)
+p.Kg = 3.7/1;
+p.M = 0.455; %kg
+p.l = 0.305; %m
+p.m = 0.210; %kg
+p.r = 0.635*10^-2; %m
+p.g = 9.81; %m/s^2
+
+end
